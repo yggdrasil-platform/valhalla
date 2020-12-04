635e05b01c771169e58795176765c55f039ee300
Kieran O'Neill <kieran.oneill@protonmail.com>
2020-12-04 09:00:56 +0100

    chore: adds database container to tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,13 +16,33 @@ function read_env_file {
 # Main function
 ##
 function main {
+  local container_id
+
   set_vars
 
   # Read test env file
   read_env_file
 
+  # Runs postgres Docker image in the background.
+  container_id=$(docker run \
+   --name valkyrie_db_test \
+   -e POSTGRES_DB="${DB_NAME}" \
+   -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
+   -e POSTGRES_USER="${DB_USER}" \
+   -d \
+   -p "${DB_PORT}":5432 \
+   postgres:12-alpine)
+  printf "%b Running PostgreSQL container: %b\n" "${INFO_PREFIX}" "${container_id}"
+
+  # Wait for container to start.
+  sleep 2
+
   # Run tests.
   "${BIN_PATH}"/jest --runInBand
+
+  # Force remove database container.
+  printf "%b Removing PostgreSQL container: %b\n" "${INFO_PREFIX}" "${container_id}"
+  docker rm -f "${container_id}"
 }
 
 # And so, it begins...
