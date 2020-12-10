70d3da404e555cf4b083345d111017919d13d369
Kieran O'Neill <kieran.oneill@protonmail.com>
2020-12-10 08:48:13 +0100

    feat: admin users can assign roles to users

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -7,19 +7,27 @@ import { Roles } from '../constants';
 import { CreateUserInput, UpdateUserInput } from '../inputs';
 
 // Models.
-import { CheckUsername, User } from '../models';
+import { CheckUsername, Role, User } from '../models';
+
+// Services.
+import { RoleService, UserService } from '../services';
 
 // Types.
 import { Context } from '../types';
 
 // Utils.
-import { createUsername, encryptSecret } from '../utils';
+import { encryptSecret } from '../utils';
 
 @Resolver()
 export default class UserResolver {
+  constructor(
+    private readonly roleService: RoleService,
+    private readonly userService: UserService
+  ) {}
+
   @Authorized([Roles.ADMIN])
   @Mutation(() => User)
-  async createUser(@Arg('input') input: CreateUserInput): Promise<User> {
+  public async createUser(@Arg('input') input: CreateUserInput): Promise<User> {
     const username: string = `${input.firstName
       .replace(/[^a-zA-Z0-9]+/g, '')
       .toLowerCase()}.${input.lastName
@@ -30,25 +38,23 @@ export default class UserResolver {
       createdAt: new Date(),
       password: encryptSecret(input.password, process.env.ENCRYPTION_KEY),
       updatedAt: new Date(),
-      username: await createUsername(username),
+      username: await this.userService.createUsername(username),
     });
 
     return await model.save();
   }
 
   @Query(() => CheckUsername)
-  async checkUsername(
+  public async checkUsername(
     @Arg('username') username: string
   ): Promise<CheckUsername> {
-    const user: User | undefined = await User.findOne({
-      where: {
-        username,
-      },
-    });
+    const user: User | undefined = await this.userService.getByUsername(
+      username
+    );
 
     return {
       isAvailable: !user,
-      suggestion: await createUsername(username),
+      suggestion: await this.userService.createUsername(username),
     };
   }
 
@@ -56,7 +62,7 @@ export default class UserResolver {
   @Query(() => User, {
     nullable: true,
   })
-  async me(@Ctx() ctx: Context): Promise<User | undefined> {
+  public async me(@Ctx() ctx: Context): Promise<User | undefined> {
     return ctx.user;
   }
 
@@ -64,29 +70,56 @@ export default class UserResolver {
   @Mutation(() => User, {
     nullable: true,
   })
-  async update(
+  public async update(
     @Arg('input') input: UpdateUserInput,
     @Ctx() ctx: Context
   ): Promise<User | undefined> {
-    const { roles, ...otherInput } = input;
-
     if (ctx.user) {
       await User.update(
         {
           id: ctx.user.id,
         },
         {
-          ...otherInput,
+          ...input,
           updatedAt: new Date(),
         }
       );
 
-      return await User.findOne({
-        relations: ['roles'],
-        where: {
-          id: ctx.user.id,
+      return await this.userService.getById(ctx.user.id);
+    }
+
+    return undefined;
+  }
+
+  @Authorized([Roles.ADMIN])
+  @Mutation(() => User, {
+    nullable: true,
+  })
+  public async addRoles(
+    @Arg('id') id: number,
+    @Arg('roles') roles: string[]
+  ): Promise<User | undefined> {
+    const user: User | undefined = await this.userService.getById(id);
+    const newRoles: Role[] = await this.roleService.getRolesByNames(roles);
+
+    if (user) {
+      await User.update(
+        {
+          id,
         },
-      });
+        {
+          updatedAt: new Date(),
+          roles: [...newRoles, ...user.roles].reduce<Role[]>(
+            (acc, currentValue) =>
+              !acc.find((value) => value.id === currentValue.id)
+                ? [...acc, currentValue]
+                : acc,
+            []
+          ),
+        }
+      );
+
+      return await this.userService.getById(id);
     }
 
     return undefined;
@@ -96,13 +129,8 @@ export default class UserResolver {
   @Query(() => User, {
     nullable: true,
   })
-  async user(@Arg('id') id: number): Promise<User | undefined> {
-    return await User.findOne({
-      relations: ['roles'],
-      where: {
-        id,
-      },
-    });
+  public async user(@Arg('id') id: number): Promise<User | undefined> {
+    return await this.userService.getById(id);
   }
 
   @Authorized([Roles.ADMIN])
