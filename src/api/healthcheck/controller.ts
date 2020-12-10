a5b1104dc1fa0d2c7af8eb5f65ff4341a44fe5c5
Kieran O'Neill <kieran.oneill@protonmail.com>
2020-12-10 16:31:36 +0100

    chore: adds more information to healthcheck

--- src/api/healthcheck/controller.ts
+++ src/api/healthcheck/controller.ts
@@ -5,6 +5,11 @@ import BaseController from '../base/controller';
 
 export default class Controller extends BaseController {
   public get(req: Request, res: Response): void {
-    res.status(200).send('OK').end();
+    res.status(200).json({
+      environment: process.env.NODE_ENV,
+      isDatabaseConnected: this.connection.isConnected,
+      name: process.env.SERVICE_NAME,
+      version: process.env.VERSION,
+    });
   }
 }
